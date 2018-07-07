3acfbcba9561d7e49b4d3b0b8a58fd2c9ce61c4b
a <310888511@qq.com>
2018-07-07 22:50:42 +0800

    7-7.1

--- js/index.js
+++ js/index.js
@@ -1,19 +1,52 @@
 $(function(){
-    function browserRedirect() {
-        var sUserAgent = navigator.userAgent.toLowerCase();
-        var bIsIpad = sUserAgent.match(/ipad/i) == "ipad";
-        var bIsIphoneOs = sUserAgent.match(/iphone os/i) == "iphone os";
-        var bIsMidp = sUserAgent.match(/midp/i) == "midp";
-        var bIsUc7 = sUserAgent.match(/rv:1.2.3.4/i) == "rv:1.2.3.4";
-        var bIsUc = sUserAgent.match(/ucweb/i) == "ucweb";
-        var bIsAndroid = sUserAgent.match(/android/i) == "android";
-        var bIsCE = sUserAgent.match(/windows ce/i) == "windows ce";
-        var bIsWM = sUserAgent.match(/windows mobile/i) == "windows mobile";
-        if (!(bIsIpad || bIsIphoneOs || bIsMidp || bIsUc7 || bIsUc || bIsAndroid || bIsCE || bIsWM) ){
-            window.location.href="../html/index2.html"
-        } else {
-            window.location.href="../html/index3.html";
+    var mySwiper = new Swiper ('.swiper-container', {
+        direction: 'vertical',
+        loop: false,
+
+        // 如果需要分页器
+        pagination: {
+            el: '.swiper-pagination'
+        },
+
+        on:{
+            init: function(){
+                swiperAnimateCache(this); //隐藏动画元素
+                swiperAnimate(this); //初始化完成开始动画
+            },
+            slideChangeTransitionEnd: function(){
+                swiperAnimate(this); //每个slide切换结束时也运行当前slide动画
+            }
         }
-    }
-    browserRedirect();
+        //// 如果需要前进后退按钮
+        //navigation: {
+        //    nextEl: '.swiper-button-next',
+        //    prevEl: '.swiper-button-prev',
+        //},
+        //
+        //// 如果需要滚动条
+        //scrollbar: {
+        //    el: '.swiper-scrollbar',
+        //},
+    });
+    //var mySwiper = $('.zhuti').swiper({
+    //    direction: 'vertical',
+    //    mode:'horizontal',
+    //    loop: false,
+    //    autoResize:true,
+    //    pagination:'.banner .swiper-pagination',
+    //    paginationClickable :true,
+    //    autoplay : 3000
+    //});
+
+    $('.thfll').click(function(){
+        window.location.href='https://zf742354.github.io/fll/html/index4.html'
+    });
+
+    $('.qrj').click(function(){
+        window.location.href='https://zf742354.github.io/qrj/html/index.html'
+    });
+
+
+
+
 });
